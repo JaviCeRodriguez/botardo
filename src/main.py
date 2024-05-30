7df7be1f223d6cc8d05fc8280c14cee69d6d194a
JaviCeRodriguez <javicerodriguez@gmail.com>
2024-05-30 01:14:08 -0300

    fix: var env read

--- src/main.py
+++ src/main.py
@@ -1,14 +1,19 @@
 # This example requires the 'message_content' privileged intents
 
 import os
+import logging
+
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
 
+log = logging.getLogger("main")
+load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
-
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 @bot.event
 async def on_ready():
@@ -25,4 +30,4 @@ async def hello(ctx):
     await ctx.send("Choo choo! 🚅")
 
 
-bot.run(os.environ["DISCORD_TOKEN"])
+bot.run(TOKEN)
